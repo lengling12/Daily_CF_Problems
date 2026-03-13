894b41e6a943cc53f55d1d59260e96ecd5f59a81
lengling12 <2810740334@qq.com>
2026-03-13 23:49:55 +0800

    Refactor and clean up cf106415f_lengling.cpp

--- daily_problems/2026/03/0313/personal_submission/cf106415f_lengling.cpp
+++ daily_problems/2026/03/0313/personal_submission/cf106415f_lengling.cpp
@@ -1,33 +1,3 @@
-#include <bits/stdc++.h>
-#define itn int
-#define int long long
-#define ind long double
-#define yes cout << "Yes"
-#define no cout << "No"
-#define pii pair<long long, long long>
-#define all(x) (x).begin(), (x).end()
-#define pci pair<char, int>
-#define re return;
-#define rep(l, i, r) for (int i = l; i <= r; i++)
-#define lep(r, i, l) for (int i = r; i >= l; i--)
-using namespace std;
-const int N = 4e5 + 10;
-const int M = 2e3 + 10;
-const int mod = 1e9 + 7;
-const int inf = 0x3f3f3f3f3f3f3f3f;
-int x, y, t, n, z, k, m, sx, sy, u;
-char ch1, ch2;
-int fx, fy;
-int num1, num2, num3, sum1, sum2, sum0, num;
-multiset<int> st2, st1, st3;
-int dx[4] = {0, -1, 0, 1}, dy[4] = {-1, 0, 1, 0}; // 上下左右
-map<int, int> mp1, mp2, mp3;
-map<int, int> mp;
-string s1, s2, s3;
-int a[N], b[N], c[N], d[N];
-int maxn, minn, sum, ans, cnt;
-vector<pii> e[N];
-int fa[N];
 void solve()
 {
     cin >> n;
@@ -70,21 +40,4 @@ void solve()
         cout << "! " << ans << endl;
         cin >> ch;
     }
-}
-
-signed main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int T;
-    T = 1;
-
-    /* cin >> T; */
-    while (T--)
-    {
-        solve();
-    }
-
-    return 0;
 }
